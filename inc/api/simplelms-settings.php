f33074fabd193b55530c8710ffbcb7cd61e01982
Xavi <x.vandalen@outlook.com>
2023-10-24 19:15:01 +0200

    Task(Submenus): Added a way to add submenus

--- inc/api/simplelms-settings.php
+++ inc/api/simplelms-settings.php
@@ -12,24 +12,59 @@ namespace SimpleLMS\API;
  */
 class SimpleLMSSettings
 {
-    private $admin_pages = array();
+    private $admin_pages = [];
+    private $admin_subpages = [];
 
     public function register()
     {
         if (!empty($this->admin_pages)) {
-            add_action("admin_menu", array($this, "add_admin_menu"));
+            add_action("admin_menu", [$this, "add_admin_menu"]);
         }
     }
 
     /**
      * Add page to the settings menu for SimpleLMS.
-     * @param array $page  Page to add.
+     * @param array $pages  Pages to add.
      * @return `SimpleLMSSettings` Reference to the updated object.
      */
     public function add_pages(array $pages)
     {
         $this->admin_pages = $pages;
+        return $this;
+    }
 
+    /**
+     * Add subpage to the settings menu for SimpleLMS.
+     * @param array $subpages  Subpages to add.
+     * @return `SimpleLMSSettings` Reference to the updated object.
+     */
+    public function add_subpages(array $subpages)
+    {
+        $this->admin_subpages = array_merge($this->admin_subpages, $subpages);
+        return $this;
+    }
+
+    function with_subpage(string $menu_title = null)
+    {
+        // We don't have any pages, so no need for a sub-page.
+        if (empty($this->admin_pages)) {
+            return $this;
+        }
+
+        // Get a reference to the first page
+        $admin_page = $this->admin_pages[0];
+        $subpage = [
+            [
+                "parent_slug" => $admin_page["slug"],
+                "page_title" => $admin_page["page_title"],
+                "menu_title" => ($menu_title) ? $menu_title : $admin_page["menu_title"],
+                "capability" => $admin_page["capability"],
+                "slug" => $admin_page["slug"],
+                "callback" => $admin_page["callback"]
+            ]
+        ];
+
+        $this->admin_subpages = $subpage;
         return $this;
     }
 
@@ -40,6 +75,10 @@ class SimpleLMSSettings
     {
         foreach ($this->admin_pages as $page) {
             add_menu_page($page['page_title'], $page['menu_title'], $page['capability'], $page['slug'], $page['callback'], $page['icon_url'], $page['position']);
+        }
+
+        foreach ($this->admin_subpages as $page) {
+            add_submenu_page($page['parent_slug'], $page['page_title'], $page['menu_title'], $page['capability'], $page['slug'], $page['callback']);
         }
     }
 }
--- inc/views/simplelms-admin.php
+++ inc/views/simplelms-admin.php
@@ -17,6 +17,7 @@ class SimpleLMSAdmin
 {
     private $settings;
     private $pages = [];
+    private $subpages = [];
 
     function __construct()
     {
@@ -33,13 +34,47 @@ class SimpleLMSAdmin
                 },
                 "icon_url" => "dashicons-welcome-learn-more",
                 "position" => 59 // Right after the seperator, before `Appearance`
-            ],
+            ]
         ];
+
+        $this->subpages = [
+            [
+                "parent_slug" => "simplelms",
+                "page_title" => "Custom Post Types",
+                "menu_title" => "Custom Post Types",
+                "capability" => "manage_options",
+                "slug" => "simplelms_cpt",
+                "callback" => function () {
+                    echo "<h1>Custom Post Types</h1>";
+                }
+            ],
+            [
+                "parent_slug" => "simplelms",
+                "page_title" => "Courses",
+                "menu_title" => "Courses",
+                "capability" => "manage_options",
+                "slug" => "simplelms_courses",
+                "callback" => function () {
+                    echo "<h1>Courses</h1>";
+                }
+            ],
+            [
+                "parent_slug" => "simplelms",
+                "page_title" => "Configuration",
+                "menu_title" => "Configuration",
+                "capability" => "manage_options",
+                "slug" => "simplelms_config",
+                "callback" => function () {
+                    echo "<h1>Configuration</h1>";
+                }
+            ]
+        ];
+
     }
 
     public function register()
     {
         // add_action("admin_menu", array($this, "add_admin_menu"));
-        $this->settings->add_pages($this->pages)->register();
+        $this->settings->add_pages($this->pages)->with_subpage("Dashboard")->add_subpages($this->subpages)->register();
     }
 }
